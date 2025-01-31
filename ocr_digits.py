ffabff73ad413e8c0f4eaf7a90009be7924cedf5
uhofmann <udo.hofmann@kurtzersa.de>
2025-01-31 11:01:30 +0100

    Enhance OCR processing by adding image erosion and inversion steps

--- ocr_digits.py
+++ ocr_digits.py
@@ -6,6 +6,10 @@
 import pytesseract
 import argparse
 import cv2
+import numpy as np
+
+# Specify the path to the Tesseract executable
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -18,14 +22,30 @@ args = vars(ap.parse_args())
 # load the input image, convert it from BGR to RGB channel ordering,
 # and initialize our Tesseract OCR options as an empty string
 image = cv2.imread(args["image"])
-rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+rgb = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Input Image", rgb)  # Add a window name
+cv2.waitKey(0)  # Wait for a key press to close the window
+
+# Apply erosion and dilation
+kernel = np.ones((11, 11), np.uint8)
+
+# Erode the image
+eroded = cv2.erode(rgb, kernel, iterations=1)
+cv2.imshow("Eroded Image", eroded)
+cv2.waitKey(0)
+
+eroded_inv = cv2.bitwise_not(eroded)
+cv2.imshow('inverted', eroded_inv)
+cv2.waitKey(0)
+
+# Use the processed image for OCR
 options = ""
 
 # check to see if *digit only* OCR should be performed, and if so,
 # update our Tesseract OCR options
 if args["digits"] > 0:
-	options = "outputbase digits"
+	options = "--oem 3 --psm 6 outputbase digits"
 
 # OCR the input image using Tesseract
-text = pytesseract.image_to_string(rgb, config=options)
+text = pytesseract.image_to_string(eroded, config=options)
 print(text)
